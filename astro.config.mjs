364e64ced27b75d8f53e35706f67293708f85347
Valeria <lerik13@gmail.com>
2024-01-16 11:26:55 -0700

    prepare for Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
 import icon from "astro-icon";
-import Compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		astroImageTools,
-		icon(),
-		Compress()
+		icon()
 	],
 });
