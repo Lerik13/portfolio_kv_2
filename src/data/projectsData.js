8c0de1a71e19e44869e765555563ec8a683600c5
Valeria <lerik13@gmail.com>
2024-01-15 17:05:36 -0700

    section About

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,7 @@ const projectsData = [
 		"name": "Sweets Catalog - Bakery Shop",
 		"link_demo": "https://sweets-alla.netlify.app/",
 		"link_code": "https://github.com/Lerik13/sweets-by-alla",
-		"technologies": "Astro framework, React, MongoDB, Node.js (express), Tailwind CSS, Figma",
+		"technologies": "Astro framework, React.js, MongoDB, Node.js, Tailwind CSS",
 		"description": "Multi-page web-application with responsive web design (mobile-first approach) for offering sweet home-made treats. All products, categories, reviews, info for hero block are storing in MongoDB. Application is SSR (everything built in time by deafault, except static pages with adapter)"
 	},
 	{
@@ -12,13 +12,13 @@ const projectsData = [
 		"name": "Portfolio Photographer - Photo Galary",
 		"link_demo": "https://ta-photograph.netlify.app",
 		"link_code": "https://github.com/Lerik13/photograph-portfolio-tanya",
-		"technologies": "Astro framework, Tailwind CSS, Figma",
+		"technologies": "Astro framework, HTML5, JavaScript, Tailwind CSS, JSON",
 		"description": "Multi-page website with responsive web design (mobile-first approach) for presenting works of photographer. Request from the client was to show all useful information on the main page (like landing page) with the opportunity to observe different photo session options on additional pages."
 	},
 	{
 		"image": "/projects/magic-artstudio.jpg",
 		"name": "Magic Art Studio - Event Agency",
-		"technologies": "HTML/CSS, SASS, GULP, jQuery, slick.js, PhotoShop",
+		"technologies": "HTML5, CSS3, SASS, GULP, JavaScript, jQuery, Slick.js, PhotoShop",
 		"link_demo": "https://magic-artstudio.com",
 		"link_code": "https://github.com/Lerik13/magic-artstudio",
 		"description": "Multi-page website for Event Agency. The main idea was create bright and attractive web-product, enable clients to get all the information from main page and to make an order from any page."
@@ -28,7 +28,7 @@ const projectsData = [
 		"name": "Piano Teacher Website",
 		"link_demo": "https://victoriapiano.ca",
 		"link_code": "https://github.com/Lerik13/piano-teacher/tree/victoria",
-		"technologies": "Vite, JS, CSS, HTML, JSON",
+		"technologies": "Vite, JavaScript, HTML5, CSS3, JSON",
 		"description": "Multilanguage Landing page for piano teacher with information about her teaching method, 'About' block, 'options for students' and Contact Form. Additional feature is Light&Dark mode."
 	},
 	{
@@ -36,7 +36,7 @@ const projectsData = [
 		"name": "Online Shopping application - Gift Shop",
 		"link_demo": "https://gift-shop-mern.onrender.com",
 		"link_code": "https://github.com/Lerik13/myshop-mern",
-		"technologies": "MongoDB, React, Node.js (express), Bootstrap CSS",
+		"technologies": "MongoDB, React.js, JavaScript, Node.js, Express.js, Bootstrap",
 		"description": "Client-Server app for selling products online by Paypal. Authorized customer can buy a product (adding the product to the Shopping Cart) if the product is not out of stock and also leave one review with star-rating."
 	},
 ]
