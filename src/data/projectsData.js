09311a84399adf3fd6d10b3f4132d7022e05cb54
Valeria <lerik13@gmail.com>
2024-01-16 20:34:37 -0700

    edit text

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -13,7 +13,7 @@ const projectsData = [
 		"link_demo": "https://ta-photograph.netlify.app",
 		"link_code": "https://github.com/Lerik13/photograph-portfolio-tanya",
 		"technologies": "Astro framework, HTML5, JavaScript, Tailwind CSS, JSON",
-		"description": "Multi-page website with responsive web design (mobile-first approach) for presenting works of photographer. Request from the client was to show all useful information on the main page (like landing page) with the opportunity to observe different photo session options on additional pages."
+		"description": "Multi-page website with responsive web design (mobile-first approach) for presenting the work of photographer. Request from the client was to show all useful information on the main page with the opportunity to observe different photo session options on additional pages."
 	},
 	{
 		"image": "/projects/magic-artstudio.jpg",
@@ -21,7 +21,7 @@ const projectsData = [
 		"technologies": "HTML5, CSS3, SASS, GULP, JavaScript, jQuery, Slick.js, PhotoShop",
 		"link_demo": "https://magic-artstudio.com",
 		"link_code": "https://github.com/Lerik13/magic-artstudio",
-		"description": "Multi-page website for Event Agency. The main idea was create bright and attractive web-product, enable clients to get all the information from main page and to make an order from any page."
+		"description": "Multi-page website for Event Agency. The main idea was to create vivid and attractive web-product, to give clients the possibility to skim through all of the significant information from the main page and to make an order."
 	},
 	{
 		"image": "/projects/victoria-piano.jpg",
@@ -29,7 +29,7 @@ const projectsData = [
 		"link_demo": "https://victoriapiano.ca",
 		"link_code": "https://github.com/Lerik13/piano-teacher/tree/victoria",
 		"technologies": "Vite, JavaScript, HTML5, CSS3, JSON",
-		"description": "Multilanguage Landing page for piano teacher with information about her teaching method, 'About' block, 'options for students' and Contact Form. Additional feature is Light&Dark mode."
+		"description": 'Multilanguage Landing page for piano teacher with information about her teaching method, "About" block, "options for students" and Contact Form. Additional feature is Light&Dark mode.'
 	},
 	{
 		"image": "/projects/gift-shop.jpg",
@@ -37,7 +37,7 @@ const projectsData = [
 		"link_demo": "https://gift-shop-mern.onrender.com",
 		"link_code": "https://github.com/Lerik13/myshop-mern",
 		"technologies": "MongoDB, React.js, JavaScript, Node.js, Express.js, Bootstrap",
-		"description": "Client-Server app for selling products online by Paypal. Authorized customer can buy a product (adding the product to the Shopping Cart) if the product is not out of stock and also leave one review with star-rating."
+		"description": "Client-Server app for selling products online by Paypal. Authorized customer can buy a product (add the product to the Shopping Cart) if the product is not out of stock and also leave one review with star-rating."
 	},
 ]
 export default projectsData;
