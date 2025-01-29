e1e329fa6185068349b7387230318620f2be8d73
Valeria <lerik13@gmail.com>
2025-01-28 17:26:13 -0700

    update Projects, pdf-file

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,43 +1,72 @@
 const projectsData = [
-	{
-		"image": "/projects/sweets-alla.jpg",
-		"name": "Sweets Catalog - Bakery Shop",
-		"link_demo": "https://sweets-alla.netlify.app/",
-		"link_code": "https://github.com/Lerik13/sweets-by-alla",
-		"technologies": "Astro framework, React.js, MongoDB, Node.js, Tailwind CSS",
-		"description": "Multi-page web-application with responsive web design (mobile-first approach) for offering sweet home-made treats. All products, categories, reviews, info for hero block are storing in MongoDB. Application is SSR (everything built in time by deafault, except static pages with adapter)"
-	},
-	{
-		"image": "/projects/photograph-tanya.jpg",
-		"name": "Portfolio Photographer - Photo Galary",
-		"link_demo": "https://ta-photograph.netlify.app",
-		"link_code": "https://github.com/Lerik13/photograph-portfolio-tanya",
-		"technologies": "Astro framework, HTML5, JavaScript, Tailwind CSS, JSON",
-		"description": "Multi-page website with responsive web design (mobile-first approach) for presenting the work of photographer. Request from the client was to show all useful information on the main page with the opportunity to observe different photo session options on additional pages."
-	},
-	{
-		"image": "/projects/magic-artstudio.jpg",
-		"name": "Magic Art Studio - Event Agency",
-		"technologies": "HTML5, CSS3, SASS, GULP, JavaScript, jQuery, Slick.js, PhotoShop",
-		"link_demo": "https://magic-artstudio.com",
-		"link_code": "https://github.com/Lerik13/magic-artstudio",
-		"description": "Multi-page website for Event Agency. The main idea was to create vivid and attractive web-product, to give clients the possibility to skim through all of the significant information from the main page and to make an order."
-	},
-	{
-		"image": "/projects/victoria-piano.jpg",
-		"name": "Piano Teacher Website",
-		"link_demo": "https://victoria-piano.netlify.app",
-		"link_code": "https://github.com/Lerik13/piano-teacher/tree/victoria",
-		"technologies": "Vite, JavaScript, HTML5, CSS3, JSON",
-		"description": 'Multilanguage Landing page for piano teacher with information about her teaching method, "About" block, "options for students" and Contact Form. Additional feature is Light&Dark mode.'
-	},
-	{
-		"image": "/projects/gift-shop.jpg",
-		"name": "Online Shopping application - Gift Shop",
-		"link_demo": "https://gift-shop-mern.onrender.com",
-		"link_code": "https://github.com/Lerik13/myshop-mern",
-		"technologies": "MongoDB, React.js, JavaScript, Node.js, Express.js, Bootstrap",
-		"description": "Client-Server app for selling products online by Paypal. Authorized customer can buy a product (add the product to the Shopping Cart) if the product is not out of stock and also leave one review with star-rating."
-	},
+  {
+    image: '/projects/sweets-alla.jpg',
+    name: 'Sweets Catalog - Bakery Shop',
+    link_demo: 'https://sweets-alla.netlify.app/',
+    link_code: 'https://github.com/Lerik13/sweets-by-alla',
+    technologies:
+      'Astro framework, React.js, MongoDB, Node.js, Tailwind CSS, Figma',
+    description:
+      'Multi-page web-application with responsive web design (mobile-first approach) for offering sweet home-made treats. All products, categories, reviews, info for hero block are storing in MongoDB. Application is SSR (everything built in time by deafault, except static pages with adapter)',
+  },
+  {
+    image: '/projects/photograph-tanya.jpg',
+    name: 'Portfolio Photographer - Photo Galary',
+    link_demo: 'https://ta-photograph.netlify.app',
+    link_code: 'https://github.com/Lerik13/photograph-portfolio-tanya',
+    technologies:
+      'Astro framework, HTML5, JavaScript, Tailwind CSS, JSON, Figma',
+    description:
+      'Multi-page website with responsive web design (mobile-first approach) for presenting the work of photographer. Request from the client was to show all useful information on the main page with the opportunity to observe different photo session options on additional pages.',
+  },
+  {
+    image: '/projects/magic-artstudio.jpg',
+    name: 'Magic Art Studio - Event Agency',
+    technologies:
+      'HTML5, CSS3, SASS, GULP, JavaScript, jQuery, Slick.js, PhotoShop, Figma',
+    link_demo: 'https://magic-artstudio.com',
+    link_code: 'https://github.com/Lerik13/magic-artstudio',
+    description:
+      'Multi-page website for Event Agency. The main idea was to create vivid and attractive web-product, to give clients the possibility to skim through all of the significant information from the main page and to make an order.',
+  },
+  {
+    image: '/projects/mountain-villa.jpg',
+    name: 'Mountain Villa - Small Hotel',
+    technologies:
+      'React, Supabase, Styled Components, Vite, React Query, Recharts, React Hook Form',
+    link_demo: 'https://mountain-villa.netlify.app',
+    link_code: 'https://github.com/Lerik13/react-hotel-app',
+    description:
+      'Mountain Villa is a full-stack application that simplifies hotel operations by offering tools for managing reservations, guest information, and user authorization. With an intuitive interface, it streamlines booking, guest record maintenance, and staff access management. Its flexible architecture makes it ideal for properties of any size, optimizing day-to-day operations.',
+  },
+  {
+    image: '/projects/victoria-piano.jpg',
+    name: 'Piano Teacher Website',
+    link_demo: 'https://victoria-piano.netlify.app',
+    link_code: 'https://github.com/Lerik13/piano-teacher/tree/victoria',
+    technologies: 'Vite, JavaScript, HTML5, CSS3, JSON, Figma',
+    description:
+      'A responsive website for a piano teacher, featuring her teaching methods, an "About" section, lesson options for students, and a user-friendly Contact Form. The site also includes a light/dark mode feature and a multilingual option for a more personalized and accessible experience.',
+  },
+  {
+    image: '/projects/gift-shop.jpg',
+    name: 'Online Shopping application - Gift Shop',
+    link_demo: 'https://gift-shop-mern.onrender.com',
+    link_code: 'https://github.com/Lerik13/myshop-mern',
+    technologies:
+      'MongoDB, React.js, JavaScript, Node.js, Express.js, Bootstrap',
+    description:
+      'Client-Server app for selling products online by Paypal. Authorized customer can buy a product (add the product to the Shopping Cart) if the product is not out of stock and also leave one review with star-rating.',
+  },
+  {
+    image: '/projects/maria-bubble_show.jpg',
+    name: 'Maria Bubble Show - Entertainment Service',
+    technologies:
+      'Astro framework, HTML5, JavaScript, Tailwind CSS, JSON, Figma',
+    link_demo: 'https://maria-bubble-show.netlify.app',
+    link_code: 'https://github.com/Lerik13/masha-bubble-show',
+    description:
+      'Maria`s Bubble Show is a responsive multipage website that highlights the fun and captivating bubble entertainment service. It features sections like Client Reviews, FAQ, booking options, a photo gallery, and a contact form, making it easy for users to explore the service and book their own bubble show.',
+  },
 ]
-export default projectsData;
+export default projectsData
